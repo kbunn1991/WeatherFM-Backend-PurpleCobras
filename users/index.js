25a032e98288325471ee7b7e323c89ad6b891a46
Kevin <ketsang12@gmail.com>
2018-09-25 15:14:29 -0400

    stashing

--- users/index.js
+++ users/index.js
@@ -1,5 +1,6 @@
-'use strict';
-const User = require('../db/models/userSchema');
-const {router} = require('./router');
 
-module.exports = {User, router};
+const User = require('../db/models/userSchema');
+const {router: usersRouter} = require('./router');
+const {router: playlistRouter} = require('./playlist');
+
+module.exports = {User, usersRouter, };
---
+++ users/playlist.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const User = require('../db/models/userSchema');
+
+const router = express.Router();
+
+router.post('/playlist', (req, res, next) => {
+
+});
