841019e0e149fe85d49b6c64c2b0eb95ffd04c60
Kevin <ketsang12@gmail.com>
2018-09-25 15:31:26 -0400

    dummy router data added for playlist

--- routers/playlist.js
+++ routers/playlist.js
@@ -5,5 +5,9 @@ const User = require('../db/models/userSchema');
 const router = express.Router();
 
 router.post('/playlist', (req, res, next) => {
-
+  return User.find()
+    .then(users => res.json(users.map(user => user)))
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
 });
+
+module.exports = {router};
