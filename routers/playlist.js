999035a4eac0d2c62a312b64ad6227e3037675a2
Kevin <ketsang12@gmail.com>
2018-09-25 15:49:42 -0400

    playlists get all call working

--- routers/playlist.js
+++ routers/playlist.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const User = require('../db/models/userSchema');
 
 const router = express.Router();
 
-router.post('/playlist', (req, res, next) => {
-  return User.find()
-    .then(users => res.json(users.map(user => user)))
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
+//seed data password for user1 is password
+
+//get all playlists for one specific user
+router.get('/playlist', (req, res, next) => {
+  const userId = req.user.id;
+  console.log(userId);
+  return User.findById(userId)
+    .then(users => {
+      console.log(users.playlists);
+      console.log(users.playlists.Sunny);
+      res.json(users.playlists);
+    })
+    .catch(err => 
+      res.status(500).json({message: 'Internal server error'}));
 });
 
 module.exports = {router};
