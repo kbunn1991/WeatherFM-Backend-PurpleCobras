816cf9beff296d47f54981fbb3ffaac1e027fa70
Ian Beihl <ianbeihl@gmail.com>
2018-09-26 09:16:58 -0700

    added delete endpoint

--- routers/playlists.js
+++ routers/playlists.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-
 const User = require('../db/models/userSchema');
 
 const router = express.Router();
@@ -9,26 +8,24 @@ const router = express.Router();
 router.use('/', passport.authenticate('jwt', { session: false, failWithError: true }));
 //seed data password for user1 is password
 
-//get all playlists for one specific user
+//---------get all playlists for one specific user
 router.get('/', (req, res, next) => {
   const userId = req.user.id;
   console.log(userId);
   return User.findById(userId)
     .then(users => {
-      console.log(users.playlists);
-      console.log(users.playlists.Sunny);
       res.json(users.playlists);
     })
     .catch(err => 
       res.status(500).json({message: 'Internal server error'}));
 });
 
-// add a song to a playlist
+//-------- add a song to a playlist
 router.put('/', (req, res, next) => {
   const {weather} = req.body;
   const {artist, songTitle, albumPng} = req.body;
   const songObj = {artist, songTitle, albumPng};
-  console.log(weather);
+  // console.log(weather);
   // console.log(songObj, '---');
   const userId = req.user.id;
   // console.log(userId, '===');
@@ -37,6 +34,30 @@ router.put('/', (req, res, next) => {
       console.log(result);
       if(result){
         result.playlists[weather].push(songObj);
+      }
+      result.save();
+      res.json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+//--------remove a song from a playlist
+router.delete('/', (req, res, next) => {
+  const {weather} = req.body;
+  const {artist, songTitle, albumPng} = req.body;
+  const songObj = {artist, songTitle, albumPng};
+  console.log(weather);
+  // console.log(songObj, '---');
+  const userId = req.user.id;
+  return User.findById(userId)
+    .then(result => {
+      if(result){
+        let newResults = result.playlists[weather].filter(song => song.songTitle !== songObj.songTitle);
+        console.log(newResults)
+        result.playlists[weather] = newResults;
+        console.log(result)
       }
       result.save();
       res.json(result);
