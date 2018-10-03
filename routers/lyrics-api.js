e40639a1aed19b095cf6013114b6632de8c99e84
Brandon Graham <bgraham452@gmail.com>
2018-10-03 12:49:24 -0400

    implemented lyrics call

--- routers/lyrics-api.js
+++ routers/lyrics-api.js
@@ -21,7 +21,9 @@ router.get('/:artist/:title', (req, res, next) => {
 
   return fetch(fetchLyricsUrl, {
     method: 'GET',
-  
+    header: {
+      'content-type': 'application/json'
+    }
   })
     .then (result => {
       //console.log(result);
